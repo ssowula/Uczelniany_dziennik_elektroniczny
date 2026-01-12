0586a5ea133f30b9b6b8dea37d1098e226128f99
Sylwester Sowula <sylwester.sowula@gmail.com>
2026-01-12 16:23:27 +0100

    utworzenie klasy abstrakcyjnej "Osoba"

---
+++ dziennik/Osoba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dziennik
+{
+    abstract class Osoba
+    {
+        int id;
+        string imie;
+        string nazwisko;
+
+        public int Id { get => id; }
+        public string Imie { get => imie; set => imie = value; }
+        public string Nazwisko { get => nazwisko; set => nazwisko = value; }
+
+        public Osoba(int id, string imie, string nazwisko) 
+        {
+            this.id = id;
+            Imie = imie;
+            Nazwisko = nazwisko;
+        }
+    }
+}
