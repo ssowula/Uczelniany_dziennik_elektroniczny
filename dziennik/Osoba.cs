0edc48c73aa5af587f5140015accc6f0b870ed6e
Sylwester Sowula <sylwester.sowula@gmail.com>
2026-01-12 17:03:06 +0100

    Utworzenie klasy dziedziczącej student oraz dodanie walidacji peselu w klasie Osoba

--- dziennik/Osoba.cs
+++ dziennik/Osoba.cs
@@ -3,24 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Runtime.Serialization;
 
 namespace dziennik
 {
+    public class ZlyPeselException : Exception
+    {
+        public ZlyPeselException()
+        {
+        }
+
+        public ZlyPeselException(string? message) : base(message)
+        {
+        }
+    }
     abstract class Osoba
     {
         int id;
         string imie;
         string nazwisko;
+        string pesel;
 
         public int Id { get => id; }
         public string Imie { get => imie; set => imie = value; }
         public string Nazwisko { get => nazwisko; set => nazwisko = value; }
+        public string Pesel { get => pesel; set 
+            {
+                if (Regex.IsMatch(value, @"^\d{11}$"))
+                {
+                    pesel = value;
+                }
+                else
+                {
+                    throw new ZlyPeselException("Podano nieprawidłowy pesel");
+                }
+            }
+        }
 
-        public Osoba(int id, string imie, string nazwisko) 
+        public Osoba() { }
+
+        public Osoba(int id, string imie, string nazwisko, string pesel)
         {
             this.id = id;
             Imie = imie;
             Nazwisko = nazwisko;
+            Pesel = pesel;
         }
     }
 }
---
+++ dziennik/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dziennik
+{
+    internal class Student : Osoba
+    {
+        static int licznik_studenci = 1;
+        string numerAlbumu;
+
+        public Student(string imie, string nazwisko) : base(licznik_studenci,imie,nazwisko)
+        {
+            licznik_studenci++;
+        }
+
+        public string utworz_album()
+        {
+            string
+        }
+
+    }
+}
