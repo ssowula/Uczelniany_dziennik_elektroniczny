6b70340fcd3c17b842e2a081db5603bfc889b607
Tomasz Król <tkrol@student.agh.edu.pl>
2026-01-12 18:22:51 +0100

    Dodano klasę Kierunek oraz metody do zarządzania kierunkami, studentami i prowadzącymi w Uczelnia. Zmieniono modyfikatory dostępu klas Ocena i Przedmiot na publiczne, dodano właściwości i poprawiono enkapsulację. Ujednolicono inicjalizację pól w konstruktorach. Zwiększono możliwości zarządzania strukturą uczelni.

---
+++ dziennik/Kierunek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dziennik
+{
+    internal class Kierunek
+    {
+        int idKierunku;
+        string nazwaKierunku;
+        List<Przedmiot> przedmioty;
+
+        public int IdKierunku { get => idKierunku; set => idKierunku = value; }
+        public string NazwaKierunku { get => nazwaKierunku; set => nazwaKierunku = value; }
+        public List<Przedmiot> Przedmioty { get => przedmioty; set => przedmioty = value; }
+        public Kierunek(int idKierunku, string nazwaKierunku)
+        {
+            IdKierunku = idKierunku;
+            NazwaKierunku = nazwaKierunku;
+            Przedmioty = new List<Przedmiot>();
+        }
+        public void DodajPrzedmiot(Przedmiot przedmiot)
+        {
+            Przedmioty.Add(przedmiot);
+        }
+        public void UsunPrzedmiot(Przedmiot przedmiot)
+        {
+            Przedmioty.Remove(przedmiot);
+        }
+
+    }
+}
--- dziennik/Ocena.cs
+++ dziennik/Ocena.cs
@@ -7,18 +7,21 @@ using System.Threading.Tasks;
 
 namespace dziennik
 {
-    internal class Ocena
+    public class Ocena
     {
-        Student student { get; }
-        Przedmiot przedmiot { get; }
+        Student student;
+        Przedmiot przedmiot;
         int wartosc;
         DateTime dataWystawienia;
         public int Wartosc { get => wartosc; set => wartosc = value; }
         public DateTime DataWystawienia { get => dataWystawienia; set => dataWystawienia = value; }
+        public Przedmiot Przedmiot { get => przedmiot; set => przedmiot = value; }
+        public Student Student { get => student; set => student = value; }
+
         public Ocena(Student student, Przedmiot przedmiot, int wartosc, DateTime dataWystawienia)
         {
-            this.student = student;
-            this.przedmiot = przedmiot;
+            Student = student;
+            Przedmiot = przedmiot;
             Wartosc = wartosc;
             DataWystawienia = dataWystawienia;
         }
