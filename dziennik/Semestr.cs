adfab9e9e69a3ba89e4b06f5a9b0e608fff812ca
Tomasz Król <tkrol@student.agh.edu.pl>
2026-01-12 18:43:54 +0100

    Zmiany w konstruktorze Kierunek, dodano klasę Semestr
    Zmieniono sposób inicjalizacji klasy Kierunek: usunięto właściwość Przedmioty oraz metody DodajPrzedmiot i UsunPrzedmiot, dodano konstruktor domyślny. Dodano nowy plik Semestr.cs z definicją enum EnumTyp oraz klasy Semestr z odpowiednimi właściwościami.

---
+++ dziennik/Semestr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dziennik
+{
+    enum EnumTyp 
+    {   Zima,
+        Lato 
+    }
+    public class Semestr
+    {
+        int rokAkademicki;
+        EnumTyp typ;
+        List<Przedmiot> przedmioty;
+
+        public int RokAkademicki { get => rokAkademicki; set => rokAkademicki = value; }
+        public List<Przedmiot> Przedmioty { get => przedmioty; set => przedmioty = value; }
+        internal EnumTyp Typ { get => typ; set => typ = value; }
+    }
+
+}
