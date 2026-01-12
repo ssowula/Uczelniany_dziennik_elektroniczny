c7a2d517987dd057aa624c137a4cd2cc12c289e6
Tomasz Król <tkrol@student.agh.edu.pl>
2026-01-12 18:04:22 +0100

    Dodano plik Ocena.cs z klasą Ocena reprezentującą ocenę studenta z przedmiotu, wraz z odpowiednimi właściwościami i konstruktorem. Dodano także plik Uczelnia.cs z pustą klasą Uczelnia jako podstawę do dalszej rozbudowy.

---
+++ dziennik/Ocena.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dziennik
+{
+    internal class Ocena
+    {
+        Student student { get; }
+        Przedmiot przedmiot { get; }
+        int wartosc;
+        DateTime dataWystawienia;
+        public int Wartosc { get => wartosc; set => wartosc = value; }
+        public DateTime DataWystawienia { get => dataWystawienia; set => dataWystawienia = value; }
+        public Ocena(Student student, Przedmiot przedmiot, int wartosc, DateTime dataWystawienia)
+        {
+            this.student = student;
+            this.przedmiot = przedmiot;
+            Wartosc = wartosc;
+            DataWystawienia = dataWystawienia;
+        }
+
+    }
+}
