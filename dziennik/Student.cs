e707494854e0c6416cf4bca0c7228c968428178f
Sylwester Sowula <sylwester.sowula@gmail.com>
2026-01-12 18:42:46 +0100

    Dodanie funkcji dodawania i usuwania studentów

--- dziennik/Student.cs
+++ dziennik/Student.cs
@@ -10,16 +10,38 @@ namespace dziennik
     {
         static int licznik_studenci = 1;
         string numerAlbumu;
-        public string NumerAlbumu { get => numerAlbumu; set => numerAlbumu = value; }
 
+        Kierunek kierunek;
+        List<Przedmiot> przedmioty;
+
+
+        public string NumerAlbumu { get => numerAlbumu; set => numerAlbumu = value; }
+        public Kierunek Kierunek { get => kierunek; set => kierunek = value; }
 
 
         public Student(string imie, string nazwisko, string pesel) : base(licznik_studenci,imie,nazwisko,pesel)
         {
             NumerAlbumu = utworz_album();
+            przedmioty = new List<Przedmiot>();
             licznik_studenci++;
         }
 
+        public void DodajPrzedmiot(Przedmiot p)
+        {
+            if (!przedmioty.Contains(p))
+            {
+                przedmioty.Add(p);
+            }
+            else
+            {
+                throw new Exception("Podany przedmiot został już dodany");
+            }
+        }
+
+        public void UsunPrzedmiot(Przedmiot p)
+        {   
+            przedmioty.Remove(p);
+        }
         public string utworz_album()
         {
             string result = licznik_studenci.ToString() + Pesel.Substring(7);
