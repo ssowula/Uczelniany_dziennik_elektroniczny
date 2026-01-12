7212c67ed6b96dd88fe6a3828e850840b8dbd1aa
Tomasz Król <tkrol@student.agh.edu.pl>
2026-01-12 17:45:43 +0100

    Dodano plik Prowadzacy.cs z klasą Prowadzacy dziedziczącą po Osoba, zawierającą tytuł naukowy, licznik oraz odpowiednie właściwości i konstruktor.

---
+++ dziennik/Prowadzacy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dziennik
+{
+    public class Prowadzacy : Osoba
+    {
+        private string tytulNaukowy;
+        static int licznik_prowadzacy = 1;
+
+        public string TytulNaukowy { get => tytulNaukowy; set => tytulNaukowy = value; }
+        public static int Licznik_prowadzacy { get => licznik_prowadzacy; set => licznik_prowadzacy = value; }
+
+        public Prowadzacy(string imie, string nazwisko, string pesel, string tytulNaukowy): base(licznik_prowadzacy, imie, nazwisko, pesel)
+        {
+            TytulNaukowy = tytulNaukowy;
+            licznik_prowadzacy++;
+        }
+    }
+}
